83e58a493f893df4b25b89bf618af936d54eb1ae
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2020-10-05 17:17:45 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://ditampol-tzuyoon.glitch.me"]
+const urls = ["https://ditaktau-nyanyi.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
